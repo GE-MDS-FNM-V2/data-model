3ae36509ba9c095e6c6444166e4053f8fdccc083
Brady Brown <bobrown101@gmail.com>
2020-02-24 20:35:36 -0500

    fix: add IDataTypeKind as direct export

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,6 +1,6 @@
 export { Action, IAction } from './Action'
 export { Choice, IChoice } from './Choice'
-export { DataType, IDataType } from './DataType'
+export { DataType, IDataType, IDataTypeKind } from './DataType'
 export { Leaf, ILeaf } from './Leaf'
 export { List, IList } from './List'
 export { Map, IMap } from './Map'
